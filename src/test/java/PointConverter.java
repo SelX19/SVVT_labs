051d2f80521339efed36bb53c393df39dbcaf6f5
SelX19 <selma.dozic@stu.ibu.edu.ba>
2024-10-31 10:52:08 +0100

    -m "labs_materials"

---
+++ src/test/java/PointConverter.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+
+public class PointConverter implements ArgumentConverter {
+    @Override
+    public Object convert(Object source, ParameterContext context) throws ArgumentConversionException {
+        String[] coords = ((String) source).split(",");
+        int x = Integer.valueOf(coords[0]);
+        int y = Integer.valueOf(coords[1]);
+        return new Point(x, y);
+    }
+}
